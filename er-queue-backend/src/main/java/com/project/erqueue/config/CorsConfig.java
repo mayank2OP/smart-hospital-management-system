7c9503db019086da3bbe6d0f0f5951a471fe835f
mayank2OP <mayankrawat460@gmail.com>
2026-05-24 22:40:54 +0530

    Update CorsConfig.java

--- er-queue-backend/src/main/java/com/project/erqueue/config/CorsConfig.java
+++ er-queue-backend/src/main/java/com/project/erqueue/config/CorsConfig.java
@@ -2,27 +2,44 @@ package com.project.erqueue.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public CorsFilter corsFilter() {
 
-        return new WebMvcConfigurer() {
+        CorsConfiguration config = new CorsConfiguration();
 
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
+        config.setAllowCredentials(true);
 
-                registry.addMapping("/**")
-                .allowedOrigins(
-                	    "http://localhost:5173",
-                	    "http://localhost:5174"
-                	)
-                        .allowedMethods("*");
-            }
-        };
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:5174",
+                "https://smart-hospital-management-system-getclmu3m.vercel.app"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        config.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
